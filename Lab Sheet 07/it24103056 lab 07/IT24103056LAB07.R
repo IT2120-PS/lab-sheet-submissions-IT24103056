1ac8d4a9db03a1de25854bed157eb7540eb0b9d4
IT24103056 <IT24103056@my.sliit.lk>
2025-09-16 13:03:15 +0530

    Add files via upload

---
+++ Lab Sheet 07/it24103056 lab 07/IT24103056LAB07.R
@@ -0,0 +1,6 @@
+setwd("C:\\Users\\nirgn\\OneDrive\\Desktop\\it24103056 lab 07")
+getwd()
+punif(256,min = 0, max = 40, lower.tail = TRUE)-punif(10,min = 0, max = 40,lower.tail=TRUE)
+pexp(2,rate = 0.333,lower.tail=TRUE)
+1-pnorm(130,mean = 100, sd=15,lower.tail=TRUE)
+qnorm(0.95,mean = 100,sd=15,lower.tail=TRUE)
